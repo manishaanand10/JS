43af365baa45121bbbc0bece105d5708b257ed3e
manishaanand10 <manishaanand461@gmail.com>
2026-01-29 07:59:16 +0000

    math.num

---
+++ introJS/01_number_math.js
@@ -0,0 +1,45 @@
+const score= 500
+//console.log(score);
+
+
+const accountvalue = new Number(400)
+// console.log(accountvalue);
+
+// console.log(accountvalue.toString().length);
+// console.log(accountvalue.toFixed(2));
+
+const value=1234.7876576;
+//console.log(value.toPrecision(3));
+
+
+/////MATH//////
+
+// console.log(Math);
+// console.log(Math.abs(-4));// convert into +ve value
+// console.log(Math.abs(5));
+// console.log(Math.min(5,6,9,10,4,7));
+// console.log(Math.max(5,6,9,10,4,7));
+// console.log(Math.round(2.9));//3
+// console.log(Math.round(3.4));
+// console.log(Math.ceil(3.4));
+// console.log(Math.floor(3.4));
+
+//console.log(Math.random());//round 0to1
+//console.log((Math.random()*10 )+ 1);
+//console.log(Math.floor(Math.random()*10 )+ 1);// single value or sab se choti value...
+
+//formula
+
+const min = 10
+const max = 20
+
+//console.log(Math.random()*(min - max + 1) + min)
+console.log(Math.floor(Math.random()*(min - max + 1)) + max)
+
+
+
+
+
+
+
+
