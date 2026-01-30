1a9dbbccd40905f87cee2aed1e353d592c3f965a
manishaanand10 <manishaanand461@gmail.com>
2026-01-30 08:07:50 +0000

    data.time

---
+++ introJS/02_date_and_time.js
@@ -0,0 +1,25 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+
+//let theCreatedDate = new Date(2026,5,27)
+//let theCreatedDate = new Date(2026,5,27,9,6)
+let theCreatedDate = new Date("01-01-2026")
+//console.log(theCreatedDate.toLocaleString());
+
+let myTime = Date.now()
+//console.log(myTime.toLocaleTimeString());
+// console.log(myTime);
+// console.log(theCreatedDate.getTime());
+ console.log(Math.floor(Date.now()/1000))//second me time
+
+ let
+
+
+
