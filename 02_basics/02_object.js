499786d673bf687c40f92327d3148f8311ef1802
manishaanand10 <manishaanand461@gmail.com>
2026-02-11 07:38:34 +0000

    object_topics

--- 02_basics/02_object.js
+++ 02_basics/02_object.js
@@ -9,8 +9,8 @@ const jsUser ={
     [mySym]:"mykey1",
     age : 20,
     phone_number: 2165641654,
-    isloggedin : false,
-    emila:"manishaanand@gmail.com"
+    isloggedin : false,   
+    email:"manishaanand@gmail.com"
 }
 
 // two way to access object
@@ -21,10 +21,33 @@ console.log(jsUser["name"])
 console.log(jsUser[mySym]) 
 
 jsUser.name="anand"
-Object.freeze(jsUser)
-
+//Object.freeze(jsUser)
 jsUser. name="isha"
-console.log(jsUser["name"]);
+console.log(jsUser);
+
+//Add function in object
+//add greeting
+
+jsUser.greeting = function(){
+    console.log("hello js")
+}
+console.log(jsUser.greeting());
+
+jsUser.greetingtwo = function(){
+    console.log(`hello, ${this.name}`);
+    
+}
+console.log(jsUser.greetingtwo());
+
+//cahtgpt
+let person = {
+  name: "Amit",
+  greet: function () {
+    return "Hello " + this.name;
+  }
+};
+
+console.log(person.greet());
 
 
 
