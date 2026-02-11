3231696865a556cebfd0b10564f2cf98c2a7c3ee
manishaanand10 <manishaanand461@gmail.com>
2026-02-11 08:52:43 +0000

    another_object_topics

---
+++ 02_basics/03_objecttwo.js
@@ -0,0 +1,82 @@
+//singleton
+
+//const  facebookUser = new Object()
+
+// non_singleton
+ const facebookUser ={}
+ facebookUser.id = "123@manisha"
+ facebookUser.name = "sunny"
+ facebookUser.isloggedin = true 
+
+ //console.log(facebookUser);
+
+ //nested object
+ const inst_User ={
+    id : "manisha_01",
+    fullname : {
+        username :{
+            fisrt_name:"manisha",
+            last_name:"anand"
+        }
+
+    }
+ }
+ console.log(inst_User.fullname.username.fisrt_name);
+
+ // object combine
+
+ let obj1 = {1:"manisha" , 2:"anand"}
+ let obj2 ={3:"diya", 4:"lodi"}
+
+
+ //let result =obj1.concat(obj2)
+ let result = Object.assign({}, obj1,obj2)
+
+ //separed value
+ //let result ={...obj1,...obj2}
+//console.log(result === obj1);
+
+
+
+ console.log(result);
+
+ const user =[
+    {
+        name:"mnisha",
+        age:20,
+        email:"mansihananad@10",
+        id:1
+    },
+     {
+        name:"mnisha",
+        age:20,
+        email:"mansihananad@10",
+        id:1
+    },
+     {
+        name:"mnisha",
+        age:20,
+        email:"mansihananad@10",
+        id:1
+    }
+ ]
+ console.log(user);
+ console.log(facebookUser);
+
+ console.log(Object.keys(facebookUser));
+ console.log(Object.values(facebookUser));
+ console.log(Object.entries(facebookUser));
+
+ console.log(facebookUser.hasownProperty('isloggedin'));
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+
+
+
+ 
