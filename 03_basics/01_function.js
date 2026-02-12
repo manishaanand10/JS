0f9a8480264abd409964fbb14c76412c56df44fc
manishaanand10 <manishaanand461@gmail.com>
2026-02-12 12:26:18 +0000

    FUNCTION

---
+++ 03_basics/01_function.js
@@ -0,0 +1,21 @@
+// console.log("M");
+// console.log("A");
+// console.log("N");
+// console.log("I");
+// console.log("S");
+// console.log("H");
+// console.log("A");
+
+function sayMyName() {
+    console.log("M");
+    console.log("A");
+    console.log("N");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+
+}
+
+sayMyName()
+
