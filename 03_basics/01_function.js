562be09bac4d3e533526ca20cab3deba086fbc04
manishaanand10 <manishaanand461@gmail.com>
2026-02-13 09:13:13 +0000

    functionvalue

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -17,5 +17,39 @@ function sayMyName() {
 
 }
 
-sayMyName()
+//sayMyName()
+
+
+function AddTwoNumber( a, b){ //parametre
+    console.log(a + b);
+}
+AddTwoNumber(2,2) //agrument
+
+// function return value
+function Add( a, b){ //parametre
+   return a+b;
+}
+let result = Add(10,30);
+console.log(result);
+
+
+//function declartion
+function greet(){
+    console.log("hello everyone" );
+    
+}
+greet();
+
+
+//parametre
+function greet(name){
+    console.log("hello everyone" + name);
+    
+}
+greet("manisha");//call to execute
+
+
+
+
+
 
