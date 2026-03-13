1d281bacb66f6221366fa5f4e97a0f12cf875d01
manishaanand10 <manishaanand461@gmail.com>
2026-03-13 08:04:00 +0000

    break condition

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,66 @@
+// switch case condition
+
+// switch (key) { 
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const weak = 10
+switch (weak) {
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    case 7:
+        console.log("sunday");
+        break;
+
+    default:
+        console.log("fir muje nhi pata ");
+        break;
+}
+
+// maths
+
+const opeartor ="-"
+const a = 10
+const b = 20
+switch (opeartor) {
+    case "+":
+        console.log(a+b);
+        break;
+    case "-":
+        console.log(a-b);
+        break;
+    case "*":
+        console.log(a*b);
+        break;
+    case "/":
+        console.log(a/b);
+        break;
+
+    default:
+        console.log("me nhi janta fir kuch");
+        
+        break;
+}
+
+ 
