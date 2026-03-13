c71d29bdb04b5f0cec5bab8cb7a9bff9e538f93a
manishaanand10 <manishaanand461@gmail.com>
2026-03-13 07:39:12 +0000

    ifelse condition

---
+++ 04_control_flow/one.js
@@ -0,0 +1,87 @@
+//// control flow
+
+if(9 != 10){
+    // console.log("true");
+    
+}
+else{
+    // console.log("false");
+    
+}
+
+const temp = 50
+if(temp < 10){
+    //console.log("less then 70");
+    
+}
+else{
+    // console.log("greater then 50");
+    
+}
+
+
+const score = 200
+if(score > 100){
+    const power ="fly"
+    // console.log(`user power:${power}`);
+    
+
+}
+//  console.log(`user power:${power}`);
+    
+ //short handling
+const balance = 440
+// if(balance > 500) console.log("test");
+
+// nested if else
+
+//  if(balance < 200 ){
+//     console.log("less then 200");
+//  }
+//  else if(balance < 500){
+//     console.log("less then 500");
+    
+//  }
+//  else if(balance < 750){
+//     console.log("less then 750");
+    
+//  }
+//  else if(balance<999){
+//     console.log("less then 999");
+    
+//  }
+
+//  else{
+//     console.log("less then 1100");
+    
+//  }
+
+// // let age = 18
+// // if(age >18){
+// //     console.log("you cannot vote");
+    
+// // }
+// else{
+//     console.log("you can");
+    
+// }
+
+// real life exmple
+
+
+const userloggedIn = true
+const upi = true
+const loggedInGoogle = false
+const fblogged =true
+
+if(userloggedIn && upi && 2==2 ){
+    console.log("allow to shopping"); 
+}  
+
+if(loggedInGoogle || fblogged){
+    console.log("user log in"); 
+}
+
+else{
+    console.log("nhai karni shopping");   
+}
