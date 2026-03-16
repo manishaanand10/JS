c7394ed3f49701d5273ab57d6a2dc87d862c92ff
manishaanand10 <manishaanand461@gmail.com>
2026-03-16 08:12:22 +0000

    while and dowhile loop

---
+++ 05_loops/two.js
@@ -0,0 +1,27 @@
+// while loop
+
+let i = 0
+while (i<=10) {
+    // console.log(`value of i is ${i}`) 
+     i=i+2
+}
+
+
+// array in while loop
+ let myarray =["manisha","isha","micki"]
+ let arr =0
+ while(arr< myarray.length){
+    // console.log(`value is ${myarray[arr]}`);
+    arr++;
+}
+
+// do while loop
+//phle kam hoga fir condition check hogi
+
+
+let marks = 11
+do {
+    console.log(`marks is ${marks}`);
+    marks++
+} while (marks<=10);
+
